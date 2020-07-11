266363d32c8ad576980ba7a6c86239642452e508
prathamn007 <58143437+prathamn007@users.noreply.github.com>
2020-07-11 15:15:30 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-import sys,os
-os.environ['TCL_LIBRARY']=r'F:/Python Install/Lib/site-packages/tcl/tcl8.6'
-os.environ['TK_LIBRARY']=r'F:/Python Install/Lib/site-packages/tcl/tk8.6'
-setup(name="main",
-      version="1.1",
-      description="Description of the app here.",
-      executables=[Executable("main.py")], requires=['pygame']
-      )
